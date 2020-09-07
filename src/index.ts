7ab51ab7c89c02c79d1de14495c957fc36ae8629
melmouk <mathilde@elmouktafi.fr>
2020-09-07 14:28:00 +0200

    Step 1 : Reading the file

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,13 @@
+import PngImage from "./png-image";
+
 (async function init() {
-   const img = new Image(1028, 464);
-   img.src = "/assets/screen.png";
+   const fileUri = "/assets/screen.png";
 
-   const canvas: HTMLCanvasElement = document.createElement("canvas");
-   canvas.width = img.width;
-   canvas.height = img.height;
+    const httpResponse = await fetch(fileUri);
+    if (!httpResponse.ok) throw new Error(`${fileUri} not found`);
 
-   const ctx = canvas.getContext("2d");
-   if (!ctx) return ;
+    const buffer: ArrayBuffer = await httpResponse.arrayBuffer();
+    const bytes: Uint8Array = new Uint8Array(buffer); // File data to decode
 
-   img.onload = () => {
-      const imgContent = [];
-      ctx.drawImage(img, 0, 0);
-      const imgData = ctx.getImageData(0, 0, img.width, img.height) || { data: [] };
-      for (let i = 0; i < imgData.data.length; i += 4) {
-          imgContent.push(new Uint8Array(imgData.data.slice(i, i + 4)));
-      }
-
-      const htmlContent = document.getElementById("content");
-      if (htmlContent) {
-         htmlContent.innerText = imgContent.join(" | ").slice(0, 2000) + "...";
-      }
-      const htmlCanvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
-      htmlCanvas.width = img.width;
-      htmlCanvas.height = img.height;
-      if (htmlCanvas) {
-          const htmlCtx = htmlCanvas.getContext("2d");
-          htmlCtx?.putImageData(imgData, 0, 0);
-      }
-   };
+    const image = new PngImage(bytes);
 })();
---
+++ src/png-image.ts
@@ -0,0 +1,7 @@
+export default class PngImage {
+    public content: Array<Uint8Array> = [];
+
+    constructor(bytes: Uint8Array) {
+
+    }
+}
